501582dafc670396436064ee10d0538bbfef2c93
ereburg <gimmethenick@gmail.com>
2021-11-03 17:41:14 +0300

    add brands

--- src/app/scripts/shared/app.ts
+++ src/app/scripts/shared/app.ts
@@ -1,9 +1,11 @@
 import { SliderManager } from '@app/shared/slider'
 import { header } from '@app/shared/header'
+import { TabsUI } from '@app/shared/tabs-brands'
 
 export const app = (): void => {
   document.addEventListener('DOMContentLoaded', () => {
     header()
     SliderManager.init('.js-slider')
+    TabsUI.init()
   })
 }
