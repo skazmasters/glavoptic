b0ebfbc623d13e7b1f808cc38363959e6de31480
ereburg <gimmethenick@gmail.com>
2021-11-07 21:30:01 +0300

    desktop improvements

--- src/app/scripts/shared/inner-sidebar/inner-sidebar.js
+++ src/app/scripts/shared/inner-sidebar/inner-sidebar.js
@@ -1,13 +1,12 @@
 import { InnerSidebarObserver } from '@app/shared/inner-sidebar/inner-sidebar-observer'
 import { InnerSidebarDesktop } from '@app/shared/inner-sidebar/inner-sidebar-desktop'
-import { InnerSidebarMobile } from '@app/shared/inner-sidebar/inner-sidebar-mobile'
 
 export class InnerSidebar {
   constructor(node) {
     node.forEach((item, i) => {
       InnerSidebarObserver.init(item, i)
       InnerSidebarDesktop.init(item)
-      InnerSidebarMobile.init(item)
+      // InnerSidebarMobile.init(item)
     })
   }
 
