1669eba62758f6d046d8dda520e3f0a75baed553
ereburg <gimmethenick@gmail.com>
2021-11-10 18:34:59 +0300

    feat: fab widget

--- src/app/scripts/shared/app.ts
+++ src/app/scripts/shared/app.ts
@@ -3,6 +3,7 @@ import { header } from '@app/shared/header'
 import { TabsUI } from '@app/shared/tabs-brands'
 import { InnerSidebar } from '@app/shared/inner-sidebar/inner-sidebar'
 import { ScrollToLink, ScrollToTop } from '@app/core/scroll-to-link'
+import { manager } from '@app/shared/popups'
 
 export const app = (): void => {
   document.addEventListener('DOMContentLoaded', () => {
@@ -12,5 +13,14 @@ export const app = (): void => {
     InnerSidebar.init(document.querySelectorAll('.js-sidebar'))
     document.querySelectorAll('.js-scroll-to').forEach((element: HTMLElement) => new ScrollToLink(element))
     document.querySelectorAll('.js-scroll-to-top').forEach((element: HTMLElement) => new ScrollToTop(element))
+
+    document.querySelectorAll('.js-popup').forEach((popup: HTMLElement): void => manager.add(popup))
+    manager.updatePopups()
+
+    const trigger = document.querySelector('.js-fab > .trigger')
+    trigger.addEventListener('click', (e: MouseEvent) => {
+      e.preventDefault()
+      ;(e.currentTarget as HTMLElement).parentElement.classList.toggle('open')
+    })
   })
 }
